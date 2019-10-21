6e4eafc25a6e89c5ceff00069bfc13190288ce6a
Thiago Capelo <thiagokpelo@gmail.com>
2019-10-20 22:52:37 -0300

    add: remove method on DAO

--- app/src/main/java/dev/thiagokpelo/agenda/dao/AlunoDAO.java
+++ app/src/main/java/dev/thiagokpelo/agenda/dao/AlunoDAO.java
@@ -12,14 +12,14 @@ public class AlunoDAO {
     private final static List<Aluno> alunos = new ArrayList<>();
     private static int contadorDeIds = 1;
 
+    private void atualizaIds() {
+        contadorDeIds++;
+    }
+
     public void salva(Aluno aluno) {
         aluno.setId(contadorDeIds);
         alunos.add(aluno);
         atualizaIds();
-    }
-
-    private void atualizaIds() {
-        contadorDeIds++;
     }
 
     public void edita(Aluno aluno) {
@@ -28,6 +28,14 @@ public class AlunoDAO {
         if (alunoEncontrado != null) {
             int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
             alunos.set(posicaoDoAluno, aluno);
+        }
+    }
+
+    public void remove(Aluno aluno) {
+        Aluno alunoEncontrado = buscaAlunoPeloId(aluno);
+
+        if (alunoEncontrado != null) {
+            alunos.remove(alunoEncontrado);
         }
     }
 
